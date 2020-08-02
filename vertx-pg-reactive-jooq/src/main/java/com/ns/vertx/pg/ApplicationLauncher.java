d1cb6ff008bd991a772a251db04581f6db321079
nixos89 <nixos89@gmail.com>
2020-08-02 17:43:30 +0200

    console output of MXBean info

--- vertx-pg-reactive-jooq/src/main/java/com/ns/vertx/pg/ApplicationLauncher.java
+++ vertx-pg-reactive-jooq/src/main/java/com/ns/vertx/pg/ApplicationLauncher.java
@@ -1,6 +1,11 @@
 package com.ns.vertx.pg;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.List;
+
 import io.vertx.core.Launcher;
+import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.ext.dropwizard.DropwizardMetricsOptions;
 
@@ -16,6 +21,19 @@ public class ApplicationLauncher extends Launcher {
 			new DropwizardMetricsOptions()
 				.setJmxEnabled(true)
 				.setJmxDomain("vertx-metrics"));
-	}		
+	}
 
+	@Override
+	public void afterStartingVertx(Vertx vertx) {
+		RuntimeMXBean rmxBean = ManagementFactory.getRuntimeMXBean();
+		String jmxPort = System.getProperty("com.sun.management.jmxremote.port");
+		System.out.println("+++++++++++ com.sun.management.jmxremote.port = " + jmxPort + " +++++++++++");
+		List<String> arguments = rmxBean.getInputArguments();
+		System.out.println("*******************************\n rmxBean.getInputArguments():");
+		for(String arg: arguments) {
+			System.out.println("arg = " + arg);
+		}
+		System.out.println("******************************");
+	}		
+	
 }
