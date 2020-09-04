3c9d9d623f431b1b834ddfc3677953778a74048b
nixos89 <nixos89@gmail.com>
2020-09-05 00:52:24 +0200

    enabled JVM metrics

--- vertx-pg-reactive-jooq/src/main/java/com/ns/vertx/pg/ApplicationLauncher.java
+++ vertx-pg-reactive-jooq/src/main/java/com/ns/vertx/pg/ApplicationLauncher.java
@@ -7,22 +7,38 @@ import java.util.List;
 import io.vertx.core.Launcher;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.json.JsonObject;
 import io.vertx.micrometer.MicrometerMetricsOptions;
 import io.vertx.micrometer.VertxJmxMetricsOptions;
 
 public class ApplicationLauncher extends Launcher {
 
+	private static final int PORT = 8080;
+	
 	public static void main(String[] args) {
 		new ApplicationLauncher().dispatch(args);
 	}
 
+	
+	private static MicrometerMetricsOptions getMetricOptions(final JsonObject config) {
+		// Performance metrics using Micrometer
+		final HttpServerOptions serverOptions = new HttpServerOptions();
+		serverOptions.setPort(PORT);
+		final JsonObject metricsConfig = config.getJsonObject("metrics", new JsonObject());
+
+		final MicrometerMetricsOptions metricsOptions = new MicrometerMetricsOptions(metricsConfig);
+		metricsOptions.setJvmMetricsEnabled(true).setEnabled(true);
+		return metricsOptions;
+	}
+	
 	@Override
 	public void beforeStartingVertx(VertxOptions options) {		
 		options.setMetricsOptions(new MicrometerMetricsOptions()
 				.setJmxMetricsOptions(new VertxJmxMetricsOptions().setEnabled(true)
 					.setStep(1)
-					.setDomain("vertx.micrometer.metrics")
-				).setEnabled(true));
+					.setDomain("vertx.micrometer.metrics")					
+				).setJvmMetricsEnabled(true).setEnabled(true));
 	}
 
 	@Override
